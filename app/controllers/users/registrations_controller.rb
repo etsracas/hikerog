77b4fae8949fb7d7adada6a5d94c2ed584b880a6
etsracas <etsryacas@gmail.com>
2018-03-11 12:23:06 +0900

    change link after sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -57,4 +57,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+    edit_user_registration_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    edit_user_session_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    edit_user_session_path
+  end
+
 end
